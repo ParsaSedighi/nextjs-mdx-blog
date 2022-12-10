368c181f8ee8c0b3711d50aeae5fc8b0fcf5bab1
ParsaSedighi <realparsasedighi@gmail.com>
2022-12-10 14:30:54 +0330

    add navbar link's styles

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,17 @@
 import Link from 'next/link';
+import styles from '../../styles/Link.module.css';
 
 const Nav = () => {
   return (
     <nav>
       <div className="flex items-center justify-center px-8 py-5 mx-8 border-b-2 border-stone-600 md:mx-16 md:justify-start">
         <div className="flex items-end space-x-8">
-          <Link className="text-3xl" href="#">
+          <Link className={'text-3xl py-1' + ` ${styles.link}`} href="#">
             MDX Blog
           </Link>
-          <Link className="hidden text-xl md:block" href="#">
+          <Link
+            className={'hidden text-xl md:block py-1' + ` ${styles.link}`}
+            href="#">
             About
           </Link>
         </div>

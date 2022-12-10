ba9b252ae725404b4cbfaca8a9a285f0181a6e33
ParsaSedighi <realparsasedighi@gmail.com>
2022-12-10 15:30:10 +0330

    Implement mdx file reading

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html>
+    <Html className="bg-gradient-to-b from-[#1a1a1a] to-[#371717] text-white">
       <Head />
-      <body className="h-screen bg-gradient-to-b from-[#1a1a1a] to-[#371717] text-white">
+      <body>
         <Main />
         <NextScript />
       </body>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,63 @@
 import Nav from '../components/Nav';
-export default function Home() {
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+import Link from 'next/link';
+import Image from 'next/image';
+
+export default function Home({ posts }) {
   return (
     <>
       <Nav />
-      <main className="mx-24 mt-7">Hello World</main>
+      <main className="mx-24 mt-7">
+        <div>
+          {posts.map((post, index) => {
+            return <Post post={post} index={index} />;
+          })}
+        </div>
+      </main>
     </>
   );
 }
+
+const Post = ({ post, index }) => {
+  return (
+    <>
+      <Link href={'/' + post.slugs} passHref key={index}>
+        <div className="flex justify-between pl-5 my-6 border border-stone-700 rounded-md bg-gradient-to-r from-[#1a1a1a] to-yellow-900 h-128">
+          <div className="flex-grow">{post.frontMatter.title}</div>
+          <div className="w-4/12">
+            <Image
+              className="rounded-r"
+              src={post.frontMatter.thumbnailUrl}
+              width="500"
+              height="400"></Image>
+          </div>
+        </div>
+      </Link>
+    </>
+  );
+};
+
+export const getStaticProps = async () => {
+  const files = fs.readdirSync(path.join('posts'));
+
+  const posts = files.map((filename) => {
+    const markdownWithMeta = fs.readFileSync(
+      path.join('posts', filename),
+      'utf-8'
+    );
+    const { data: frontMatter } = matter(markdownWithMeta);
+
+    return {
+      frontMatter,
+      slug: filename.split('.')[0],
+    };
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
